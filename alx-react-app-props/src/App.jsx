648de22a0346dcfd14875e5a419b1a6a1a4f8dd4
ggllawc4 <gracelawrence4788@gmail.com>
2024-11-06 14:41:35 +0100

    Refractor prop drilling to use context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,60 @@
+import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
+import UserProfile from './components/UserProfile';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1>Vite + React</h1>
+
+      <div>
+        <WelcomeMessage />
+      </div>
+
+      <div>
+        <ProfilePage />
+        <Header />
+        <Footer />
+        <MainContent />
+      </div>
+
+      <div>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </div>
+
+      <div className="card">
+        <button onClick={() => setCount(count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
