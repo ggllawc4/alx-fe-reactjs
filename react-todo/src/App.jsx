60e209ed21e747ed9cabe252369d112960bb6ef6
ggllawc4 <gracelawrence4788@gmail.com>
2024-11-21 10:51:51 +0100

    import TodoList in app.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,3 +1,4 @@
+import TodoList from './components/TodoList'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -15,6 +16,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div>
+      <div>
+        <TodoList />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
