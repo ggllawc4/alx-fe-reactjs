d86739e5f236997acb2684bf9cb50378daef93dd
ggllawc4 <gracelawrence4788@gmail.com>
2024-12-10 11:32:12 +0100

    Install tailwindcss

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
